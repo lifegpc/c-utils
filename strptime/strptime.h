ebdb1bd55c1e43a44da6de889c6ed5d630ace172
lifegpc <root@lifegpc.com>
2024-01-18 18:25:29 +0800

    Fix bug

--- strptime/strptime.h
+++ strptime/strptime.h
@@ -1,10 +1,18 @@
 #ifndef STRPTIME_H
 #define STRPTIME_H
 
+#if __cplusplus
+extern "C" {
+#endif
+
 #include <time.h>
 
 #ifdef _WIN32
 char* strptime(const char *buf, const char *fmt, struct tm *tm);
+#endif
+
+#if __cplusplus
+}
 #endif
 
 #endif // STRPTIME_H
